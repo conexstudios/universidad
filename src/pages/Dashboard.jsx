2f534e82bb429b7d73ab88e207e403d79593685d
Yotman Reyes <yotman.reyes@gmail.com>
2025-05-14 14:11:52 -0400

    rename component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,9 @@ import Menu from "../components/Menu";
 import Buscador from "../components/Buscador";
 import BarraInformativa from "../components/BarraInformativa";
 import WelcomeArea from "../components/WelcomeArea";
-<<<<<<< HEAD
 import HomeWork from "../components/HomeWork";
-=======
->>>>>>> 4d6e9e654fda8744fbfff6ab5b12c2b7ab61cca1
 import Salon from "../components/Salon";
-import Oro from "../components/Oro";
+import Horario from "../components/Horario";
 import Asignatura from "../components/Asignatura";
 import Pen from "./Pen";
 
@@ -20,20 +17,12 @@ const Dashboard = () => {
         <BarraInformativa></BarraInformativa>
         <Buscador></Buscador>
         <WelcomeArea></WelcomeArea>
-<<<<<<< HEAD
         <HomeWork></HomeWork>
         <BarraInformativa></BarraInformativa>
-        <Salon></Salon>
-        <Oro></Oro>
-=======
-        <BarraInformativa></BarraInformativa>
-        <Salon></Salon>
-        <Oro></Oro>
-        <Asignatura></Asignatura>
-        <Pen></Pen>
->>>>>>> 4d6e9e654fda8744fbfff6ab5b12c2b7ab61cca1
       </main>
-      <Asig></Asig>
+      <Salon></Salon>
+      <Horario></Horario>
+      <Asignatura></Asignatura>
       <Pen></Pen>
     </>
   );
