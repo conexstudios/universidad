0b58c68a8dbb998dbea7da650c5018619ce58466
Yotman Reyes <yotman.reyes@gmail.com>
2025-07-29 13:41:56 -0400

    update app

--- src/store/sessionStore.js
+++ src/store/sessionStore.js
@@ -1,8 +1,18 @@
-import { create } from 'zustand';
+import create from "zustand";
+import { persist } from "zustand/middleware";
 
-const useSessionStore = create((set) => ({
-  session: null,
-  setSession: (sessionData) => set({ session: sessionData }),
-}));
+const useSessionStore = create(
+  persist(
+    (set) => ({
+      session: {},
+      setSession: (data) => set({ session: data }),
+      clearSession: () => set({ session: {} }),
+    }),
+    {
+      name: "session-storage", 
+      getStorage: () => localStorage,
+    }
+  )
+);
 
 export default useSessionStore;
